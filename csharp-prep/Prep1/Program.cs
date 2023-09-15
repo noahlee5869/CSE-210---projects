46dcb51d9bfb6bd7931e3b7bfce761d507575764
noahlee5869 <noahblee2@gmail.com>
2023-09-15 00:11:09 -0600

    firstpractice

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+      //  string last_name = "";
+
+        Console.Write("Please enter your first name: ");
+        string first_name = Console.ReadLine();
+        Console.Write(first_name);
+        Console.WriteLine();
+        Console.Write("Please enter your last name: ");
+        string last_name = Console.ReadLine();
+        Console.WriteLine(last_name);
+        Console.Write("Your name is: ");
+        Console.Write(last_name);
+        Console.Write(" , ");
+        Console.Write(first_name);
+        Console.Write(last_name);
+
+
+
     }
 }
