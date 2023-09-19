b0f87142c72f55d973c3d8c6b7f7292008203198
noahlee5869 <noahblee2@gmail.com>
2023-09-19 13:18:38 -0600

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+            Console.WriteLine("Please enter the magic number: ");
+            string magic_number = Console.ReadLine();
+            int number = int.Parse(magic_number);
+            Console.WriteLine("What is your guess?");
+            
+            int guess;
+
+            do
+            {
+                string guess_string = Console.ReadLine();
+                guess = int.Parse(guess_string);
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You got it!");
+                }
+            } while (guess != number);
+            
     }
 }
