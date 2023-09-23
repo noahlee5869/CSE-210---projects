b9b4aaf0b58b6533498809d10d671faa8f74991f
noahlee5869 <noahblee2@gmail.com>
2023-09-23 13:25:11 -0600

    Learning 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,34 @@ using System;
 
 class Program
 {
+    public static Job job1;
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Micrsoft";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+        job1.PrintJobTitle();
+        
+    
+
+        Job job2 = new Job();
+        job2._jobTitle = "Electrical Engineer";
+        job2._company = "Boeing";
+        job2._startYear = 1999;
+        job2._endYear = 2065;
+        job2.PrintJobTitle();
+
+        Resume resume1 = new Resume();
+        resume1._personName = "Noah Lee";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
+
+
     }
 }
